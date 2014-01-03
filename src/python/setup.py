d4760b65cf8c4eaf3e8917c706d7768a60ccbb27
devttys0 <heffnercj@gmail.com>
2014-01-02 21:36:42 -0500

    Added python setup.py

---
+++ src/python/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name = "libmpsse",
+      version = "1.4 beta",
+      description = "Open source library for SPI/I2C control via FTDI chips",
+      author = "Craig Heffner",
+      url = "https://github.com/devttys0/libmpsse",
+      py_modules = ["mpsse"],
+)
