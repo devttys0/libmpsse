d9365c9235e2d1078cf01b28fbb1fc752666952f
elimohl <elimohl@gmail.com>
2019-10-04 23:48:35 +0300

    Fixed mixed indentation

--- src/examples/gpio.py
+++ src/examples/gpio.py
@@ -9,12 +9,12 @@ io = MPSSE(GPIO)
 # Toggle the first GPIO pin on/off 10 times
 for i in range(0, 10):
 
-	io.PinHigh(GPIOL0)
-	print "GPIOL0 State:", io.PinState(GPIOL0)
-	sleep(1)
+    io.PinHigh(GPIOL0)
+    print "GPIOL0 State:", io.PinState(GPIOL0)
+    sleep(1)
 
-	io.PinLow(GPIOL0)
-	print "GPIOL0 State:", io.PinState(GPIOL0)
-	sleep(1)
+    io.PinLow(GPIOL0)
+    print "GPIOL0 State:", io.PinState(GPIOL0)
+    sleep(1)
 
 io.Close()
